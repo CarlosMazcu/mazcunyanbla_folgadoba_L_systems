ed02585ef51f1bbc07c128070ddcdc1426fde2a6
mazcunyanbla <mazcunyanbla@esat-libertad.com>
2025-02-17 17:39:08 +0100

    first commit

---
+++ premake5.lua
@@ -0,0 +1,60 @@
+workspace "ProyectoLSystems"
+    configurations { "Debug", "Release" }
+    location "build"
+
+    -- Configuración para Debug y Release
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+        optimize "Off"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "Speed"
+
+project "LSystemApp"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    targetdir "bin/%{cfg.buildcfg}"
+
+    -- Archivos de código fuente
+    files {
+        "src/**.h",
+        "src/**.cpp",
+        "include/**.h"
+    }
+
+    -- Directorios de inclusión
+    includedirs {
+        "include",
+        "C:/SFML/include"
+    }
+
+    -- Bibliotecas de SFML (estáticas)
+    filter "configurations:Release"
+        links {
+            "sfml-graphics-s",
+            "sfml-window-s",
+            "sfml-system-s"
+        }
+        defines { "SFML_STATIC" }  -- Define este símbolo para compilación estática
+
+    filter "configurations:Debug"
+        links {
+            "sfml-graphics-s-d",
+            "sfml-window-s-d",
+            "sfml-system-s-d"
+        }
+        defines { "SFML_STATIC" }  -- Define este símbolo para compilación estática
+
+    -- Directorios de bibliotecas
+    libdirs {
+        "C:/SFML/lib"
+    }
+
+    -- Configuración específica para Windows
+    filter "system:windows"
+        systemversion "latest"
+        staticruntime "On"
+        defines { "PLATFORM_WINDOWS" }
